81e0dfc138072b3d0cc223f1038f484e38eec57e
Nurlibay <koshkinbayevn@gmail.com>
2023-03-15 22:40:26 +0500

    something changed

--- app/src/main/java/uz/nurlibaydev/mytaxitesttask/presetation/MainActivity.kt
+++ app/src/main/java/uz/nurlibaydev/mytaxitesttask/presetation/MainActivity.kt
@@ -1,32 +1,8 @@
 package uz.nurlibaydev.mytaxitesttask.presetation
 
-import android.Manifest
-import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
 import uz.nurlibaydev.mytaxitesttask.R
-import uz.nurlibaydev.mytaxitesttask.service.LocationService
-import uz.nurlibaydev.mytaxitesttask.utils.hasPermission
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        startService()
-    }
-
-    private fun startService() {
-        if (hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            val intent = Intent(this, LocationService::class.java)
-            ContextCompat.startForegroundService(this, intent)
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        //stopService(Intent(this, LocationService::class.java))
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
