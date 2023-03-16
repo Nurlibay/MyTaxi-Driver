0cbbc12e58992db1981655ba1ac085f2a05dfb81
Nurlibay <koshkinbayevn@gmail.com>
2023-03-16 20:10:04 +0500

    project codes optimized through ktlint

--- app/src/main/java/uz/nurlibaydev/mytaxitesttask/di/ServiceModule.kt
+++ app/src/main/java/uz/nurlibaydev/mytaxitesttask/di/ServiceModule.kt
@@ -1,19 +1,11 @@
 package uz.nurlibaydev.mytaxitesttask.di
 
-import android.app.PendingIntent
-import android.content.Context
-import androidx.core.app.NotificationCompat
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ServiceComponent
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ServiceScoped
-import uz.nurlibaydev.mytaxitesttask.R
 import uz.nurlibaydev.mytaxitesttask.service.LocationService
-import uz.nurlibaydev.mytaxitesttask.utils.Constants.CHANNEL_ID
-import uz.nurlibaydev.mytaxitesttask.utils.Constants.NOTIFICATION_CONTENT_TEXT
-import uz.nurlibaydev.mytaxitesttask.utils.Constants.NOTIFICATION_TITLE
 
 /**
  *  Created by Nurlibay Koshkinbaev on 15/03/2023 06:34
